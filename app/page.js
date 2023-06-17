86d05b8ece29cf43ed54eeeb4da0f9bc8167a1b3
Martin Macchia <mmacchia1178@gmail.com>
2023-06-17 20:32:15 -0300

    se agrego prototipo de form

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-yellow-500 justify-between p-24">
-      <h1 className="text-8xl font-bold ">
-      API MEC
-      </h1>
+      <h1 className="h-1.5 text-8xl font-bold ">API MEC</h1>
+      <SearchForm />
     </main>
-  )
+  );
 }
